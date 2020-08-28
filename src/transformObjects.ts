1829fe8af29c86a555417c888ce8e57b7329fd99
Aaron Huggins <ahuggins@nethealthshops.com>
2020-08-28 09:57:08 -0500

    Implement async iterable

--- src/transformObjects.ts
+++ src/transformObjects.ts
@@ -1,23 +1,36 @@
 import { readableStreamFrom } from './readableStreamFrom'
 import { TransformObjectStream, TOSOptions } from './TransformObjectStream'
 
-/** Promisify the entire object transformation flow. */
-export async function transformObjects<I = any, O = any> (iterable: Iterable<I>, options: TOSOptions): Promise<Iterable<O>> {
-  const results: O[] = []
+/** Promisify object transformation into an async iterable. */
+export function transformIterable <I = any, O = any> (
+  iterable: Iterable<I>,
+  options: TOSOptions
+): AsyncIterable<O> {
   const input = readableStreamFrom(iterable)
   const output = input.pipeThrough(new TransformObjectStream<I, O>(options))
   const reader = output.getReader()
-  const pump = async function pump (): Promise<O[]> {
+  const pump = async function *pump (): AsyncIterable<O> {
     const result = await reader.read()
 
-    if (result.done) {
-      return results
-    } else {
-      results.push(result.value)
+    return {
+      ...result,
+      next: pump
     }
-
-    return await pump()
   }
 
-  return await pump()
+  return pump()
+}
+
+/** Promisify the entire object transformation flow. */
+export async function transformObjects<I = any, O = any> (
+  iterable: Iterable<I>,
+  options: TOSOptions
+): Promise<Iterable<O>> {
+  const results: O[] = []
+
+  for await (const result of transformIterable<I, O>(iterable, options)) {
+    results.push(result)
+  }
+
+  return results
 }
